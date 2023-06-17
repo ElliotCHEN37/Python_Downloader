4b71d55c0f4940507f7f0e6fe12e96810bd40966
Elliot <elliotchen2010@gmail.com>
2023-06-18 00:47:43 +0800

    update to v1.2

--- Python_downloader_multi-threaded_edition.py
+++ Python_downloader_multi-threaded_edition.py
@@ -1,70 +1,64 @@
-from __future__ import annotations
-from tqdm import tqdm
+import tkinter as tk
+import webbrowser
+from tkinter import ttk, messagebox
+import threading
 import requests
-import multitasking
-import signal
-from retry import retry
-signal.signal(signal.SIGINT, multitasking.killall)
 
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
-}
+win = tk.Tk()
+win.title("Python Downloader Multi-threaded Edition v1.2 - GUI version")
+win.geometry("600x300")
+win.iconbitmap("pydl.ico")
 
-MB = 1024**2
+url_entry = tk.Entry(win, width=50)
+url_entry.pack()
 
+filename_entry = tk.Entry(win, width=50)
+filename_entry.pack()
 
-def split(start: int, end: int, step: int) -> list[tuple[int, int]]:
-    parts = [(start, min(start+step, end))
-             for start in range(0, end, step)]
-    return parts
+progress_bar = ttk.Progressbar(win, orient="horizontal", length=400, mode="determinate")
+progress_bar.pack()
 
+complete_label = tk.Label(win, text="", font=("Arial", 12))
+complete_label.pack()
 
-def get_file_size(url: str, raise_error: bool = False) -> int:
-    response = requests.head(url)
-    file_size = response.headers.get('Content-Length')
-    if file_size is None:
-        if raise_error is True:
-            raise ValueError('該文件不支援多線程下載！/ This file does not support multi-threaded download! ')
-        return file_size
-    return int(file_size)
+def download():
+    url = url_entry.get()
+    filename = filename_entry.get()
 
+    def download_thread():
+        response = requests.get(url, stream=True)
+        total_size = int(response.headers.get('content-length', 0))
+        block_size = 1024
 
-def download(url: str, file_name: str, retry_times: int = 3, each_size=16*MB) -> None:
-    f = open(file_name, 'wb')
-    file_size = get_file_size(url)
+        with open(filename, 'wb') as f:
+            downloaded_size = 0
+            for data in response.iter_content(block_size):
+                f.write(data)
+                downloaded_size += len(data)
+                progress = downloaded_size / total_size * 100
+                progress_bar["value"] = progress
+                win.update()
 
-    @retry(tries=retry_times)
-    @multitasking.task
-    def start_download(start: int, end: int) -> None:
-        _headers = headers.copy()
-        _headers['Range'] = f'bytes={start}-{end}'
-        response = session.get(url, headers=_headers, stream=True)
-        chunk_size = 128
-        chunks = []
-        for chunk in response.iter_content(chunk_size=chunk_size):
-            chunks.append(chunk)
-            bar.update(chunk_size)
-        f.seek(start)
-        for chunk in chunks:
-            f.write(chunk)
-        del chunks
+        complete_label.config(text="Download complete")
 
-    session = requests.Session()
-    each_size = min(each_size, file_size)
+    download_thread = threading.Thread(target=download_thread)
+    download_thread.start()
 
-    parts = split(0, file_size, each_size)
-    print(f'分塊數：{len(parts)}')
-    bar = tqdm(total=file_size, desc=f'下載 {file_name} 中/Downloading {file_name} | 進度/progress rate')
-    for part in parts:
-        start, end = part
-        start_download(start, end)
-    multitasking.wait_for_tasks()
-    f.close()
-    bar.close()
+download_button = tk.Button(win, text="Download", command=download)
+download_button.pack()
+
+def show_about():
+    messagebox.showinfo("About", "Python Downloader. \n\nAuthor: Elliot\nVersion: 1.2")
+
+about_button = tk.Button(win, text="About", command=show_about)
+about_button.pack()
+
+def open_link(event):
+    webbrowser.open("https://sites.google.com/view/pydl/index")
+
+link_label = tk.Label(win, text="Visit official site", cursor="hand2")
+link_label.pack()
+link_label.bind("<Button-1>", open_link)
 
 
-if "__main__" == __name__:
-    print(f"歡迎使用Python Downloader Multi-threaded Edition!/Thank you for choosing Python Downloader Multi-threaded Edition!")
-    url = input("請輸入下載鏈接/Please paste the download link here：")
-    file_name = input("請輸入完整檔案名/Please type your file name：")
-    download(url, file_name)
+win.mainloop()
--- Python_downloader_standard_edition.py
+++ Python_downloader_standard_edition.py
@@ -1,26 +1,58 @@
+import tkinter as tk
+import webbrowser
+from tkinter import ttk, messagebox
 import requests
-from tqdm import tqdm
 
-def download(url: str, file_name: str):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE'
-    }
-    head = requests.head(url, headers=headers)
-    file_size = head.headers.get('Content-Length')
-    if file_size is not None:
-        file_size = int(file_size)
-    response = requests.get(url, headers=headers, stream=True)
-    chunk_size = 1024
-    bar = tqdm(total=file_size, desc=f'下載 {file_name} 中/Downloading {file_name} | 進度/progress rate')
-    with open(file_name, mode='wb') as f:
-        for chunk in response.iter_content(chunk_size=chunk_size):
-            f.write(chunk)
-            bar.update(chunk_size)
-    bar.close()
+win = tk.Tk()
+win.title("Python Downloader Standard Edition v1.2 - GUI version")
+win.geometry("600x300")
+win.iconbitmap("pydl.ico")
 
+url_entry = tk.Entry(win, width=50)
+url_entry.pack()
 
-if "__main__" == __name__:
-    print(f"歡迎使用Python Downloader!/Thank you for choosing Python Downloader!")
-    url = input("請輸入下載鏈接/Please paste the download link here：")
-    file_name = input("請輸入完整檔案名/Please type your file name：")
-    download(url, file_name)
+filename_entry = tk.Entry(win, width=50)
+filename_entry.pack()
+
+progress_bar = ttk.Progressbar(win, orient="horizontal", length=400, mode="determinate")
+progress_bar.pack()
+
+complete_label = tk.Label(win, text="", font=("Arial", 12))
+complete_label.pack()
+
+def download():
+    url = url_entry.get()
+    filename = filename_entry.get()
+
+    response = requests.get(url, stream=True)
+    total_size = int(response.headers.get('content-length', 0))
+    block_size = 1024
+
+    with open(filename, 'wb') as f:
+        downloaded_size = 0
+        for data in response.iter_content(block_size):
+            f.write(data)
+            downloaded_size += len(data)
+            progress = downloaded_size / total_size * 100
+            progress_bar["value"] = progress
+            win.update()
+
+    complete_label.config(text="Download complete")
+
+download_button = tk.Button(win, text="Download", command=download)
+download_button.pack()
+
+def show_about():
+    messagebox.showinfo("About", "Python Downloader. \n\nAuthor: Elliot\nVersion: 1.2")
+
+about_button = tk.Button(win, text="About", command=show_about)
+about_button.pack()
+
+def open_link(event):
+    webbrowser.open("https://sites.google.com/view/pydl/index")
+
+link_label = tk.Label(win, text="Visit official site", cursor="hand2")
+link_label.pack()
+link_label.bind("<Button-1>", open_link)
+
+win.mainloop()
